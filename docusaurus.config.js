fb8e13dedb8e4760ab74e7b5c0a9a0633017e56c
Salahuddin Muhammad <salah.3@gmail.com>
2025-12-27 15:34:34 +0500

    change in docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://physical-ai-humanoid-robotics-book-bv071duft.vercel.app/docs/intro',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub Pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
